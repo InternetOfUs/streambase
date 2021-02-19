65df7e03ecfaaa5a9a9a1b9703e26442e0d94f15
Marcelo Rodas <marcelo.rodasbritez@unitn.it>
2021-02-19 11:37:04 +0100

    adding a script to push the docker image.

---
+++ docker-hub.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+SERVER="internetofus"
+MODULE_NAME="ilogbase"
+MODULE_VERSION="1.6.2"
+USERNAME="wenetsw"
+TAG="latest"
+# Read Password
+echo -n Password: 
+read -s password
+echo
+PASSWORD=$password
+
+docker build . -t $SERVER/$MODULE_NAME:$MODULE_VERSION
+
+
+if [[ $1 = "" ]]; then
+    exit 0
+else
+    if [ $1 = "push" ]; then
+        docker login -u $USERNAME -p $PASSWORD
+        docker push $SERVER/$MODULE_NAME:$MODULE_VERSION
+    fi
+fi
+
+# docker tag internetofus/ilogbase:1.1.0 internetofus/ilogbase:latest
+# docker tag $SERVER/$MODULE_NAME:$MODULE_VERSION $SERVER/$MODULE_NAME:"latest"
